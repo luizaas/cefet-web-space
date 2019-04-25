d4b6ae56fb8a2753449491597f5beb44105f99aa
aluno <aluno@cefetmg.br>
2019-04-25 11:36:41 -0300

    Exercicios 1 e 2

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,14 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let botoes = document.querySelectorAll(".botao-expandir-retrair")
+botoes.forEach( botaoAtual =>
+  botaoAtual.addEventListener("click", (botaoAtual) => {
+    let pai = botaoAtual.path[1]
+    if (pai.classList == "expandido") {
+        pai.classList = ""
+        botaoAtual.path[0].innerHTML="+"
+    } else {
+        pai.classList = "expandido"
+        botaoAtual.path[0].innerHTML="-"
+    }
+}));
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,22 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+  let atual=0;
+  document.getElementById("proximo").onclick = function () {
+      let slide = document.getElementById("slide");
+      let prox
+      if (atual==4) prox=0;
+      else prox=atual+1;
+      atual=prox;
+      slide.src=servidorDasImagens+todasAsImagens[prox];
+      console.log(prox);
+  }
+  document.getElementById("anterior").onclick = function () {
+      let slide = document.getElementById("slide");
+      if (atual==0) var ante=4;
+      else var ante=atual-1;
+      atual=ante;
+      slide.src=servidorDasImagens+todasAsImagens[ante];
+      console.log(ante);
+  }
